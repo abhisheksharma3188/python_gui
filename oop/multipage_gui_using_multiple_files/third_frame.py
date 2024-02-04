8981d601498927e1295daf6d90d7fc9ed761e7e4
abhisheksharma3188 <111152006+abhisheksharma3188@users.noreply.github.com>
2024-02-04 18:47:59 +0530

    Update third_frame.py

--- oop/multipage_gui_using_multiple_files/third_frame.py
+++ oop/multipage_gui_using_multiple_files/third_frame.py
@@ -8,7 +8,7 @@ class ThirdFrame:
         self.frame.place(x=0, y=0, relwidth=1, relheight=1)  # this adds frame to window at x=0 and y=0 and then make it acquire full height and width
         self.label = tk.Label(self.frame, text="Third frame", bg='blue', fg="white", font=("Helvetica", 16))  # this creates label in frame
         self.label.pack(pady=30)  # this displays label in frame
-        self.button = tk.Button(self.frame, text="Main", command=self.open_main_frame)
+        self.button = tk.Button(self.frame, text="Main", command=self.open_main_frame) # this creates button in frame
         self.button.pack()
 
     def get_frame(self):
