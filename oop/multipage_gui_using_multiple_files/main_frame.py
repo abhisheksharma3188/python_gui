161bcb63dc4feffd2b5aa9dd55158990eb5af592
abhisheksharma3188 <111152006+abhisheksharma3188@users.noreply.github.com>
2024-02-04 18:31:56 +0530

    Update and rename frame1.py to main_frame.py

---
+++ oop/multipage_gui_using_multiple_files/main_frame.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+
+
+class MainFrame:
+    def __init__(self, root):
+        self.frames_dictionary = None
+        self.frame = tk.Frame(root,bg="red")
+        self.frame.place(x=0, y=0, relwidth=1, relheight=1)  # this adds frame to window at x=0 and y=0 and then make it acquire full height and width
+        self.label = tk.Label(self.frame, text="Main frame", bg='red', fg="white", font=("Helvetica", 16))  # this creates label in frame
+        self.label.pack(pady=30)  # this displays label in frame
+        self.button = tk.Button(self.frame, text="Main", command=self.open_main_frame)
+        self.button.pack()
+
+    def get_frame(self):
+        return self.frame
+
+    def set_frames_dictionary(self, frames_dictionary):
+        self.frames_dictionary = frames_dictionary
+
+    def open_main_frame(self):
+        self.frames_dictionary["main_frame"].tkraise()
