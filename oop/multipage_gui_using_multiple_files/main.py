b11bedcf883c0b5787c2cf78690a3857e01535ee
abhisheksharma3188 <111152006+abhisheksharma3188@users.noreply.github.com>
2024-02-04 18:45:56 +0530

    Update main.py

--- oop/multipage_gui_using_multiple_files/main.py
+++ oop/multipage_gui_using_multiple_files/main.py
@@ -16,25 +16,25 @@ class App:
         self.root.config(menu=self.menu_bar)  # this adds menu to window
         # code to create menu above
 
-        self.frames_dictionary={}
+        self.frames_dictionary = {} # this sets dictionary of frames which is sent to all frame classes so that even frame classes have function to switch frames
 
-        self.main_frame_obj = MainFrame(self.root)  # this creates object of Frame1 class
-        self.main_frame = self.main_frame_obj.get_frame()
-        self.frames_dictionary["main_frame"]=self.main_frame
+        self.main_frame_obj = MainFrame(self.root)  # this creates object of MainFrame class
+        self.main_frame = self.main_frame_obj.get_frame() # this gets frame so that it can be used in open_frame function
+        self.frames_dictionary["main_frame"] = self.main_frame # this fills frames dictionary
 
-        self.second_frame_obj = SecondFrame(self.root)  # this creates object of Frame1 class
-        self.second_frame = self.second_frame_obj.get_frame()
-        self.frames_dictionary["insert_frame"] = self.second_frame
+        self.second_frame_obj = SecondFrame(self.root)  # this creates object of SecondFrame class
+        self.second_frame = self.second_frame_obj.get_frame() # this gets frame so that it can be used in open_frame function
+        self.frames_dictionary["insert_frame"] = self.second_frame # this fills frames dictionary
 
-        self.third_frame_obj = ThirdFrame(self.root)  # this creates object of Frame1 class
-        self.third_frame = self.third_frame_obj.get_frame()
-        self.frames_dictionary["view_frame"] = self.third_frame
+        self.third_frame_obj = ThirdFrame(self.root)  # this creates object of ThirdFrame class
+        self.third_frame = self.third_frame_obj.get_frame() # this gets frame so that it can be used in open_frame function
+        self.frames_dictionary["view_frame"] = self.third_frame # this fills frames dictionary
 
-        self.main_frame_obj.set_frames_dictionary(self.frames_dictionary)
-        self.second_frame_obj.set_frames_dictionary(self.frames_dictionary)
-        self.third_frame_obj.set_frames_dictionary(self.frames_dictionary)
+        self.main_frame_obj.set_frames_dictionary(self.frames_dictionary) # this sets frame dictionary for objects of frames
+        self.second_frame_obj.set_frames_dictionary(self.frames_dictionary) # this sets frame dictionary for objects of frames
+        self.third_frame_obj.set_frames_dictionary(self.frames_dictionary) # this sets frame dictionary for objects of frames
 
-        self.open_frame(self.main_frame)
+        self.open_frame(self.main_frame) # this opens main_frame in beginning
 
         self.root.mainloop()
 
