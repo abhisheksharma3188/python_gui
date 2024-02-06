7f35aa6ce4fe7c68f86c8acfacc6e004fc17bd4a
abhisheksharma3188 <111152006+abhisheksharma3188@users.noreply.github.com>
2024-02-06 11:59:28 +0530

    Update second_frame.py

--- oop/multipage_gui_using_multiple_files/second_frame.py
+++ oop/multipage_gui_using_multiple_files/second_frame.py
@@ -3,19 +3,24 @@ import tkinter as tk
 
 class SecondFrame:
     def __init__(self, root):
-        self.frames_dictionary = None
+        self.objects_dictionary = None
         self.frame = tk.Frame(root,bg="green")
         self.frame.place(x=0, y=0, relwidth=1, relheight=1)  # this adds frame to window at x=0 and y=0 and then make it acquire full height and width
         self.label = tk.Label(self.frame, text="Second frame", bg='green', fg="white", font=("Helvetica", 16))  # this creates label in frame
-        self.label.pack(pady=30)  # this displays label in frame
-        self.button = tk.Button(self.frame, text="Main", command=self.open_main_frame) # this creates button in frame
-        self.button.pack()
+        self.label.pack(pady=20)  # this displays label in frame
+        self.button1 = tk.Button(self.frame, text="Main", command=self.open_main_frame) # this creates button in frame
+        self.button1.pack(pady=(0,10))
+        self.button2 = tk.Button(self.frame, text="Third", command=self.open_third_frame)  # this creates button in frame
+        self.button2.pack()
 
-    def get_frame(self):
+    def get_frame(self):  # when other classes call this function using object of this class they get the frame
         return self.frame
 
-    def set_frames_dictionary(self, frames_dictionary):
-        self.frames_dictionary = frames_dictionary
+    def set_objects_dictionary(self, objects_dictionary):  # when other classes call this function using object of this class they can set objects dictionary of this class
+        self.objects_dictionary = objects_dictionary
 
     def open_main_frame(self):
-        self.frames_dictionary["main_frame"].tkraise()
+        self.objects_dictionary["main_frame_obj"].get_frame().tkraise()
+
+    def open_third_frame(self):
+        self.objects_dictionary["third_frame_obj"].get_frame().tkraise()
